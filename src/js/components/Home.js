47d00c6c6b97948a2747e91fdc52a580499aa377
mattyrichards <matt@kreative.co.uk>
2016-05-11 00:13:15 +0100

    Home page complete

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -66,60 +66,65 @@ class Home extends React.Component {
               </div>
               <a className="c-masthead__footer" href="#" onClick={this.clickMasthead}>Hide careers</a>
               <div className={careerClass}>
+                
                 <div className="c-career__item c-career__item--left">
-                  <div className="c-career__heading l-spacing-bottom">Order</div>
-                  
-                  <div className="c-career__subheading l-spacing-bottom">Empire</div>
-                  <div className="c-career__list">
-                    {Object.keys(this.state.careers).map((key) => { return this.renderCareers(key, 'Empire') })}
-                  </div>
-
-                  <div className="c-career__subheading l-spacing-bottom">High Elves</div>
-                  <div className="c-career__list">
-                    {Object.keys(this.state.careers).map((key) => { return this.renderCareers(key, 'High Elf') })}
-                  </div>
-
-                  <div className="c-career__subheading l-spacing-bottom">Dwarves</div>
-                  <div className="c-career__list">
-                    {Object.keys(this.state.careers).map((key) => { return this.renderCareers(key, 'Dwarf') })}
+                  <div className="c-career__heading l-spacing-bottom">Order</div>  
+                  <div className="l-row l-row--justify l-row--wrap">
+                    <div className="c-career__list">
+                      <div className="c-career__subheading l-spacing-bottom">Empire</div>
+                      {Object.keys(this.state.careers).map((key) => { return this.renderCareers(key, 'Empire') })}
+                    </div>                    
+                    <div className="c-career__list">
+                      <div className="c-career__subheading l-spacing-bottom">High Elves</div>
+                      {Object.keys(this.state.careers).map((key) => { return this.renderCareers(key, 'High Elf') })}
+                    </div>
+                    <div className="c-career__list">
+                      <div className="c-career__subheading l-spacing-bottom">Dwarves</div>
+                      {Object.keys(this.state.careers).map((key) => { return this.renderCareers(key, 'Dwarf') })}
+                    </div>
                   </div>
 
                 </div>
                 <div className="c-career__item c-career__item--right">
-                  <div className="c-career__heading">Destruction</div>
-                  
-                  <div className="c-career__subheading">Chaos</div>
-                  <div className="c-career__list">
-                    {Object.keys(this.state.careers).map((key) => { return this.renderCareers(key, 'Chaos') })}
+                  <div className="c-career__heading l-spacing-bottom">Destruction</div>
+                  <div className="l-row l-row--justify l-row--wrap">
+                    <div className="c-career__list">
+                      <div className="c-career__subheading l-spacing-bottom">Chaos</div>
+                      {Object.keys(this.state.careers).map((key) => { return this.renderCareers(key, 'Chaos') })}
+                    </div>
+                    <div className="c-career__list">
+                      <div className="c-career__subheading l-spacing-bottom">Dark Elves</div>
+                      {Object.keys(this.state.careers).map((key) => { return this.renderCareers(key, 'Dark Elf') })}
+                    </div>
+                    <div className="c-career__list">
+                      <div className="c-career__subheading l-spacing-bottom">Greenskins</div>
+                      {Object.keys(this.state.careers).map((key) => { return this.renderCareers(key, 'Greenskin') })}
+                    </div>
                   </div>
-
-                  <div className="c-career__subheading">Dark Elves</div>
-                  <div className="c-career__list">
-                    {Object.keys(this.state.careers).map((key) => { return this.renderCareers(key, 'Dark Elf') })}
-                  </div>
-
-                  <div className="c-career__subheading">Greenskins</div>
-                  <div className="c-career__list">
-                    {Object.keys(this.state.careers).map((key) => { return this.renderCareers(key, 'Greenskin') })}
-                  </div>
-
                 </div>
               </div>
             </div>
           </div>
           <div className="pure-g">
-              <div className="pure-u-1-3">
-                <p>Latest updates</p>
-                <p>RoR Career Builder currently in beta</p>
+            <div className="pure-u-1-3">
+              <div className="c-list l-spacing-top--large l-spacing-left">
+                <div className="c-list__heading">Latest updates</div>
+                <p className="c-list__item">- Currently in beta</p>
               </div>
-              <div className="pure-u-2-3">
-                <p>About RoR Career Builder. Github link. Link to RoR.</p>
-
+            </div>
+            <div className="pure-u-2-3">
+              <div className="l-page-copy l-spacing-top--large">
+                <p>Warhammer Online: Age of Reckoning is back. 
+                Resurrected by volunteers on a private server, we now have <a className="l-page-copy__link" href="http://www.returnofreckoning.com" target="blank">Return of Reckoning</a>. 
+                Inspired by the great work of these developers comes RoR Career Builder.</p>
+                <p>This web app aims to replace and improve upon the great work done by <a className="l-page-copy__link" href="http://waronlinebuilder.org" target="blank">Warhammer Online Career Builder</a> back in 2013.</p>
+                <p>Details of updates will appear on this page.</p>
+                <p>All code is available on <i className="fa fa-github"></i> <a href="#" className="l-page-copy__link" target="blank">Github</a>. Feel free to fork, contribute, <a href="#" className="l-page-copy__link" target="blank">raise bugs</a> and <a href="#" className="l-page-copy__link" target="blank">make suggestions</a>.</p>
               </div>
+            </div>
           </div>
         </div>
       </div>
-      
     );
   }
 }
