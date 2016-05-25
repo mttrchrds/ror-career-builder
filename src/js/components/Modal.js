cc4fe11642dc05e5313d8ab46bf088338ad57986
mattyrichards <matt@kreative.co.uk>
2016-05-25 21:54:30 +0100

    ESLint complete

--- src/js/components/Modal.js
+++ src/js/components/Modal.js
@@ -2,43 +2,38 @@ import React from 'react';
 import classNames from 'classnames';
 import '../../scss/components/Modal.scss';
 
-class Modal extends React.Component {
-
-  constructor(props) {
-    super(props);
-    // Bind functions early. More performant. Upgrade to autobind when Babel6 sorts itself out
-    this.closeModal = this.closeModal.bind(this);
-  }
-
-  closeModal() {
-    this.props.updateOverlayVisibility(false);
-    this.props.updateModalVisibility(false);
-  }
-
-  render() {
-    let modalClass = classNames({
-      'c-modal': true,
-      'c-modal--visible': this.props.modal.visible,
-    });
-    return (
-      <div className={modalClass}>
-        <div className="c-modal__overlay" onClick={this.closeModal}></div>
-        <div className="c-modal__window" onClick={this.clickWindow}>
-          <div className="c-modal__title">
-            {this.props.modal.contentTitle}
-          </div>
-          <div className="c-modal__content u-margin__bottom--large">
-            {this.props.modal.contentBody}
-          </div>
-          <div className="c-modal__footer o-row o-row--right">
-            <button className="pure-button c-button c-button--primary" type="button" onClick={this.closeModal}>
-              Close
-            </button>
-          </div>
+const Modal = (props) => {
+  const modalClass = classNames({
+    'c-modal': true,
+    'c-modal--visible': props.modal.visible,
+  });
+  const closeModal = () => {
+    props.updateOverlayVisibility(false);
+    props.updateModalVisibility(false);
+  };
+  return (
+    <div className={modalClass}>
+      <div className="c-modal__window">
+        <div className="c-modal__title">
+          {props.modal.contentTitle}
+        </div>
+        <div className="c-modal__content u-margin__bottom--large">
+          {props.modal.contentBody}
+        </div>
+        <div className="c-modal__footer o-row o-row--right">
+          <button className="pure-button c-button c-button--primary" type="button" onClick={closeModal}>
+            Close
+          </button>
         </div>
       </div>
-    )
-  }
-}
+    </div>
+  );
+};
+
+Modal.propTypes = {
+  updateOverlayVisibility: React.PropTypes.func,
+  updateModalVisibility: React.PropTypes.func,
+  modal: React.PropTypes.object,
+};
 
 export default Modal;
