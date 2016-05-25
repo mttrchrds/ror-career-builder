665f96977884f8d9de9e4f7be1b072b7acadc269
mattyrichards <matt@kreative.co.uk>
2016-05-25 23:11:20 +0100

    ESLint progress

--- src/js/components/CareerTitle.js
+++ src/js/components/CareerTitle.js
@@ -3,7 +3,7 @@ import '../../scss/components/CareerTitle.scss';
 
 class CareerTitle extends React.Component {
   render() {
-    let url = `/images/icons/${this.props.careerShort}.png`;
+    const url = `/images/icons/${this.props.careerShort}.png`;
     return (
       <div className="u-title-height u-margin__bottom--large">
         <img src={url} className="c-title__icon"></img>
@@ -11,8 +11,13 @@ class CareerTitle extends React.Component {
           {this.props.career.name}
         </h1>
       </div>
-    )
+    );
   }
 }
+
+CareerTitle.propTypes = {
+  careerShort: React.PropTypes.string,
+  career: React.PropTypes.object,
+};
 
 export default CareerTitle;
