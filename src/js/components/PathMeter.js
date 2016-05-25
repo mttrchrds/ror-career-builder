a7a02eb0bc5e2a1b6e53d7a00cee5b3551b0d7ee
mattyrichards <matt@kreative.co.uk>
2016-05-25 21:17:56 +0100

    ESLint complete

--- src/js/components/PathMeter.js
+++ src/js/components/PathMeter.js
@@ -1,99 +1,122 @@
 import React from 'react';
 import AbilityMastery from './AbilityMastery';
 
-class PathMeter extends React.Component {
+const moraleRank = 4;
 
-  render() {
-    const moraleRank = 4;
-    return (
-      <div>
-        <div className="o-column u-margin__left--small">
-          <div className="u-margin__bottom--tiny">
-            <AbilityMastery pathMeter={this.props.pathMeter}
-              masteryPoints={this.props.masteryPoints}
-              updateMasteryPoints={this.props.updateMasteryPoints}
-              details={this.props.pathOptionalAbilities.lvl7}
-              setUserSelectionMorale={this.props.setUserSelectionMorale}
-              userSelections={this.props.userSelections}
-              moraleRank={moraleRank}
-              setSelectedAbilities={this.props.setSelectedAbilities}
-              selectedAbilities={this.props.selectedAbilities}
-              masteryPath={this.props.masteryPath} />
-          </div>
-          <div className="u-margin__bottom--tiny">
-            <AbilityMastery pathMeter={this.props.pathMeter}
-              masteryPoints={this.props.masteryPoints}
-              updateMasteryPoints={this.props.updateMasteryPoints}
-              details={this.props.pathOptionalAbilities.lvl6}
-              setSelectedAbilities={this.props.setSelectedAbilities}
-              selectedAbilities={this.props.selectedAbilities}
-              setUserSelectionMasteryAbilities={this.props.setUserSelectionMasteryAbilities}
-              userSelections={this.props.userSelections}
-              masteryPath={this.props.masteryPath} />
-          </div>
-          <div className="u-margin__bottom--tiny">
-            <AbilityMastery pathMeter={this.props.pathMeter}
-              masteryPoints={this.props.masteryPoints}
-              updateMasteryPoints={this.props.updateMasteryPoints}
-              details={this.props.pathOptionalAbilities.lvl5}
-              setSelectedAbilities={this.props.setSelectedAbilities}
-              selectedAbilities={this.props.selectedAbilities}
-              currentTacticLimit={this.props.currentTacticLimit}
-              setUserSelectionTactic={this.props.setUserSelectionTactic}
-              userSelections={this.props.userSelections}
-              masteryPath={this.props.masteryPath} />
-          </div>
-          <div className="u-margin__bottom--tiny">
-            <AbilityMastery pathMeter={this.props.pathMeter}
-              masteryPoints={this.props.masteryPoints}
-              updateMasteryPoints={this.props.updateMasteryPoints}
-              details={this.props.pathOptionalAbilities.lvl4}
-              setSelectedAbilities={this.props.setSelectedAbilities}
-              selectedAbilities={this.props.selectedAbilities}
-              setUserSelectionMasteryAbilities={this.props.setUserSelectionMasteryAbilities}
-              userSelections={this.props.userSelections}
-              masteryPath={this.props.masteryPath} />
-          </div>
-          <div className="u-margin__bottom--tiny">
-            <AbilityMastery pathMeter={this.props.pathMeter}
-              masteryPoints={this.props.masteryPoints}
-              updateMasteryPoints={this.props.updateMasteryPoints}
-              details={this.props.pathOptionalAbilities.lvl3}
-              setSelectedAbilities={this.props.setSelectedAbilities}
-              selectedAbilities={this.props.selectedAbilities}
-              currentTacticLimit={this.props.currentTacticLimit}
-              setUserSelectionTactic={this.props.setUserSelectionTactic}
-              userSelections={this.props.userSelections}
-              masteryPath={this.props.masteryPath} />
-          </div>
-          <div className="u-margin__bottom--tiny">
-            <AbilityMastery pathMeter={this.props.pathMeter}
-              masteryPoints={this.props.masteryPoints}
-              updateMasteryPoints={this.props.updateMasteryPoints}
-              details={this.props.pathOptionalAbilities.lvl2}
-              setSelectedAbilities={this.props.setSelectedAbilities}
-              selectedAbilities={this.props.selectedAbilities}
-              setUserSelectionMasteryAbilities={this.props.setUserSelectionMasteryAbilities}
-              userSelections={this.props.userSelections}
-              masteryPath={this.props.masteryPath} />
-          </div>
-          <div className="u-margin__bottom--tiny">
-            <AbilityMastery pathMeter={this.props.pathMeter}
-              masteryPoints={this.props.masteryPoints}
-              updateMasteryPoints={this.props.updateMasteryPoints}
-              details={this.props.pathOptionalAbilities.lvl1}
-              setSelectedAbilities={this.props.setSelectedAbilities}
-              selectedAbilities={this.props.selectedAbilities}
-              currentTacticLimit={this.props.currentTacticLimit}
-              setUserSelectionTactic={this.props.setUserSelectionTactic}
-              userSelections={this.props.userSelections}
-              masteryPath={this.props.masteryPath} />
-          </div>
-        </div>
+const PathMeter = (props) =>
+  <div>
+    <div className="o-column u-margin__left--small">
+      <div className="u-margin__bottom--tiny">
+        <AbilityMastery
+          pathMeter={props.pathMeter}
+          masteryPoints={props.masteryPoints}
+          updateMasteryPoints={props.updateMasteryPoints}
+          details={props.pathOptionalAbilities.lvl7}
+          setUserSelectionMorale={props.setUserSelectionMorale}
+          userSelections={props.userSelections}
+          moraleRank={moraleRank}
+          setSelectedAbilities={props.setSelectedAbilities}
+          selectedAbilities={props.selectedAbilities}
+          masteryPath={props.masteryPath}
+        />
       </div>
-    )
-  }
+      <div className="u-margin__bottom--tiny">
+        <AbilityMastery
+          pathMeter={props.pathMeter}
+          masteryPoints={props.masteryPoints}
+          updateMasteryPoints={props.updateMasteryPoints}
+          details={props.pathOptionalAbilities.lvl6}
+          setSelectedAbilities={props.setSelectedAbilities}
+          selectedAbilities={props.selectedAbilities}
+          setUserSelectionMasteryAbilities={props.setUserSelectionMasteryAbilities}
+          userSelections={props.userSelections}
+          masteryPath={props.masteryPath}
+        />
+      </div>
+      <div className="u-margin__bottom--tiny">
+        <AbilityMastery
+          pathMeter={props.pathMeter}
+          masteryPoints={props.masteryPoints}
+          updateMasteryPoints={props.updateMasteryPoints}
+          details={props.pathOptionalAbilities.lvl5}
+          setSelectedAbilities={props.setSelectedAbilities}
+          selectedAbilities={props.selectedAbilities}
+          currentTacticLimit={props.currentTacticLimit}
+          setUserSelectionTactic={props.setUserSelectionTactic}
+          userSelections={props.userSelections}
+          masteryPath={props.masteryPath}
+        />
+      </div>
+      <div className="u-margin__bottom--tiny">
+        <AbilityMastery 
+          pathMeter={props.pathMeter}
+          masteryPoints={props.masteryPoints}
+          updateMasteryPoints={props.updateMasteryPoints}
+          details={props.pathOptionalAbilities.lvl4}
+          setSelectedAbilities={props.setSelectedAbilities}
+          selectedAbilities={props.selectedAbilities}
+          setUserSelectionMasteryAbilities={props.setUserSelectionMasteryAbilities}
+          userSelections={props.userSelections}
+          masteryPath={props.masteryPath}
+        />
+      </div>
+      <div className="u-margin__bottom--tiny">
+        <AbilityMastery
+          pathMeter={props.pathMeter}
+          masteryPoints={props.masteryPoints}
+          updateMasteryPoints={props.updateMasteryPoints}
+          details={props.pathOptionalAbilities.lvl3}
+          setSelectedAbilities={props.setSelectedAbilities}
+          selectedAbilities={props.selectedAbilities}
+          currentTacticLimit={props.currentTacticLimit}
+          setUserSelectionTactic={props.setUserSelectionTactic}
+          userSelections={props.userSelections}
+          masteryPath={props.masteryPath}
+        />
+      </div>
+      <div className="u-margin__bottom--tiny">
+        <AbilityMastery
+          pathMeter={props.pathMeter}
+          masteryPoints={props.masteryPoints}
+          updateMasteryPoints={props.updateMasteryPoints}
+          details={props.pathOptionalAbilities.lvl2}
+          setSelectedAbilities={props.setSelectedAbilities}
+          selectedAbilities={props.selectedAbilities}
+          setUserSelectionMasteryAbilities={props.setUserSelectionMasteryAbilities}
+          userSelections={props.userSelections}
+          masteryPath={props.masteryPath}
+        />
+      </div>
+      <div className="u-margin__bottom--tiny">
+        <AbilityMastery
+          pathMeter={props.pathMeter}
+          masteryPoints={props.masteryPoints}
+          updateMasteryPoints={props.updateMasteryPoints}
+          details={props.pathOptionalAbilities.lvl1}
+          setSelectedAbilities={props.setSelectedAbilities}
+          selectedAbilities={props.selectedAbilities}
+          currentTacticLimit={props.currentTacticLimit}
+          setUserSelectionTactic={props.setUserSelectionTactic}
+          userSelections={props.userSelections}
+          masteryPath={props.masteryPath}
+        />
+      </div>
+    </div>
+  </div>;
 
-}
+PathMeter.propTypes = {
+  pathMeter: React.PropTypes.number,
+  pathOptionalAbilities: React.PropTypes.obj,
+  masteryPoints: React.PropTypes.number,
+  updateMasteryPoints: React.PropTypes.func,
+  setSelectedAbilities: React.PropTypes.func,
+  selectedAbilities: React.PropTypes.array,
+  currentTacticLimit: React.PropTypes.number,
+  setUserSelectionTactic: React.PropTypes.func,
+  userSelections: React.PropTypes.array,
+  masteryPath: React.PropTypes.string,
+  setUserSelectionMasteryAbilities: React.PropTypes.func,
+  setUserSelectionMorale: React.PropTypes.func,
+};
 
 export default PathMeter;
