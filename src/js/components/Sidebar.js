190f932e718c4ac3d3d63fc800b52ad2cc3547a6
mattyrichards <matt@kreative.co.uk>
2016-05-26 21:43:28 +0100

    ESLint complete

--- src/js/components/Sidebar.js
+++ src/js/components/Sidebar.js
@@ -3,37 +3,27 @@ import classNames from 'classnames';
 import { Link } from 'react-router';
 import '../../scss/components/Sidebar.scss';
 
-class Sidebar extends React.Component {
-
-  constructor(props) { 
-    super(props); 
-    // Bind functions early. More performant. Upgrade to autobind when Babel6 sorts itself out
-    this.renderCareer = this.renderCareer.bind(this);
-  }
-
-  renderCareer(key) {
-    const career = this.props.careers[key];
+const Sidebar = (props) => {
+  const renderCareer = (key) => {
+    const career = props.careers[key];
     const url = `/career/${key}`;
     const imgUrl = `/images/icons/${key}.png`;
     return (
       <Link key={key} className="c-sidebar__item" to={url}><img src={imgUrl} className="c-title__icon c-title__icon--tiny" />{career.name}</Link>
     );
-  }
-
-  render() {
-    const sidebarClass = classNames({
-      'c-sidebar': true,
-      'c-sidebar--active': this.props.sidebar.visible,
-    });
-    return (
-      <div className={sidebarClass}>
-        <div className="c-sidebar__content">
-          {Object.keys(this.props.careers).map(this.renderCareer)}
-        </div>
+  };
+  const sidebarClass = classNames({
+    'c-sidebar': true,
+    'c-sidebar--active': props.sidebar.visible,
+  });
+  return (
+    <div className={sidebarClass}>
+      <div className="c-sidebar__content">
+        {Object.keys(props.careers).map(renderCareer)}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 Sidebar.propTypes = {
   careers: React.PropTypes.object,
