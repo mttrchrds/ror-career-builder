a283c73b7e653d8703e853712b93cb82b5cdfe53
mattyrichards <matt@kreative.co.uk>
2016-05-26 21:32:23 +0100

    ESLint complete

--- src/js/components/Breadcrumb.js
+++ src/js/components/Breadcrumb.js
@@ -2,40 +2,30 @@ import React from 'react';
 import { Link } from 'react-router';
 import '../../scss/components/Breadcrumb.scss';
 
-class Breadcrumb extends React.Component {
-
-  constructor(props) {
-    super(props);
-    // Bind functions early. More performant. Upgrade to autobind when Babel6 sorts itself out
-    this.clickBreadcrumb = this.clickBreadcrumb.bind(this);
-  }
-
-  clickBreadcrumb(e) {
+const Breadcrumb = (props) => {
+  const clickBreadcrumb = (e) => {
     e.preventDefault();
-    this.props.updateOverlayVisibility(true);
-    this.props.updateSidebarVisibility(true);
-  }
-
-  render() {
-    return (
-      <div className="u-margin__bottom--large">
-        <span className="c-breadcrumb">
-          <Link to="/" className="c-breadcrumb__link">Home</Link>
-        </span>
-        <span className="c-breadcrumb c-breadcrumb__divider"><i className="fa fa-angle-right"></i></span>
-        <span className="c-breadcrumb">
-          <a href="#" onClick={this.clickBreadcrumb} className="c-breadcrumb__link">{this.props.career.faction}</a>
-        </span>
-        <span className="c-breadcrumb c-breadcrumb__divider"><i className="fa fa-angle-right"></i></span>
-        <span className="c-breadcrumb">
-          <a href="#" onClick={this.clickBreadcrumb} className="c-breadcrumb__link">{this.props.career.race}</a>
-        </span>
-        <span className="c-breadcrumb c-breadcrumb__divider"><i className="fa fa-angle-right"></i></span>
-        <span className="c-breadcrumb c-breadcrumb--active">{this.props.career.class}</span>
-      </div>
-    );
-  }
-}
+    props.updateOverlayVisibility(true);
+    props.updateSidebarVisibility(true);
+  };
+  return (
+    <div className="u-margin__bottom--large">
+      <span className="c-breadcrumb">
+        <Link to="/" className="c-breadcrumb__link">Home</Link>
+      </span>
+      <span className="c-breadcrumb c-breadcrumb__divider"><i className="fa fa-angle-right"></i></span>
+      <span className="c-breadcrumb">
+        <a href="#" onClick={clickBreadcrumb} className="c-breadcrumb__link">{props.career.faction}</a>
+      </span>
+      <span className="c-breadcrumb c-breadcrumb__divider"><i className="fa fa-angle-right"></i></span>
+      <span className="c-breadcrumb">
+        <a href="#" onClick={clickBreadcrumb} className="c-breadcrumb__link">{props.career.race}</a>
+      </span>
+      <span className="c-breadcrumb c-breadcrumb__divider"><i className="fa fa-angle-right"></i></span>
+      <span className="c-breadcrumb c-breadcrumb--active">{props.career.class}</span>
+    </div>
+  );
+};
 
 Breadcrumb.propTypes = {
   updateOverlayVisibility: React.PropTypes.func,
