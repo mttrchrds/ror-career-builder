ff7625fac71e1bbd10a66e884c583e0637545604
Matt Richards <matt@kreative.co.uk>
2017-11-03 13:29:03 +0000

    Sane ordering of careers in sidebar.

--- src/js/components/Sidebar.js
+++ src/js/components/Sidebar.js
@@ -4,25 +4,63 @@ import { Link } from 'react-router';
 import css from '../../css/components/Sidebar.css';
 
 const Sidebar = (props) => {
-  console.log('careers', props.careers);
-  // Convert careers object into an array so we can sort
+  
   let careersArray = [];
+  let dwarfArray = [];
+  let empireArray = [];
+  let highElfArray = [];
+  let chaosArray = [];
+  let greenskinArray = [];
+  let darkElfArray = [];
+
+  // Convert careers object into separate arrays for each race
   for(let key in props.careers) {
     let tmpObj = {};
     tmpObj.name = props.careers[key].name;
     tmpObj.race = props.careers[key].race;
     tmpObj.faction = props.careers[key].faction;
     tmpObj.shortName = key;
-    careersArray.push(tmpObj);
+    switch (tmpObj.race) {
+      case 'Dwarf':
+        dwarfArray.push(tmpObj);
+        break;
+      case 'High Elf':
+        highElfArray.push(tmpObj);
+        break;
+      case 'Empire':
+        empireArray.push(tmpObj);
+        break;
+      case 'Greenskin':
+        greenskinArray.push(tmpObj);
+        break;
+      case 'Chaos':
+        chaosArray.push(tmpObj);
+        break;
+      case 'Dark Elf':
+        darkElfArray.push(tmpObj);
+        break;
+    }
   }
-  console.log('careers array before', careersArray);
-  // Sort array
-  careersArray.sort(function(a,b) {
-    console.log('a', a.race);
-    console.log('b', b.race);
-    return a.race > b.race;
-  });
-  console.log('careers array after', careersArray);
+  
+  function compareShortName(a,b) {
+    if (a.shortName < b.shortName)
+      return -1;
+    if (a.shortName > b.shortName)
+      return 1;
+    return 0;
+  }
+  
+  // Sort arrays alphabetically
+  dwarfArray.sort(compareShortName);
+  highElfArray.sort(compareShortName);
+  empireArray.sort(compareShortName);
+  greenskinArray.sort(compareShortName);
+  chaosArray.sort(compareShortName);
+  darkElfArray.sort(compareShortName);
+  
+  // Join arrays into single beast
+  careersArray = empireArray.concat(dwarfArray, highElfArray, chaosArray, greenskinArray, darkElfArray);
+
   const renderItem = (obj) => {
     const clickItem = () => {
       props.gaCareerSelected(obj.shortName);
@@ -34,12 +72,13 @@ const Sidebar = (props) => {
         <img src={imgUrl} className={css.icon} />{obj.name}
       </Link>
     );
-    //<SidebarItem key={key[0]} gaCareerSelected={props.gaCareerSelected} shortName={key[0]} careerName={key[1].name} />
   }
+
   const sidebarClass = classNames({
     [css.container]: !props.sidebar.visible,
     [css.containerActive]: props.sidebar.visible,
   });
+
   return (
     <div className={sidebarClass}>
       <div className={css.content}>
