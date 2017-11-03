c32a57e02ed3b4fe005ea14e923a5454b03db1c6
Matt Richards <matt@kreative.co.uk>
2017-11-03 00:09:02 +0000

    WIP ordering sidebar

--- src/js/components/Sidebar.js
+++ src/js/components/Sidebar.js
@@ -3,28 +3,39 @@ import classNames from 'classnames';
 import { Link } from 'react-router';
 import css from '../../css/components/Sidebar.css';
 
-const SidebarItem = (props) => {
-  const clickItem = () => {
-    props.gaCareerSelected(props.shortName);
-  };
-  const url = `/career/${props.shortName}`;
-  const imgUrl = `/images/icons/${props.shortName}.png`;
-  return (
-    <Link className={css.item} to={url} onClick={clickItem}>
-      <img src={imgUrl} className={css.icon} />{props.careerName}
-    </Link>
-  );
-};
-
-SidebarItem.propTypes = {
-  careerName: React.PropTypes.string,
-  shortName: React.PropTypes.string,
-  gaCareerSelected: React.PropTypes.func,
-};
-
 const Sidebar = (props) => {
-  const renderItem = (key) =>
-    <SidebarItem key={key} gaCareerSelected={props.gaCareerSelected} shortName={key} careerName={props.careers[key].name} />;
+  console.log('careers', props.careers);
+  // Convert careers object into an array so we can sort
+  let careersArray = [];
+  for(let key in props.careers) {
+    let tmpObj = {};
+    tmpObj.name = props.careers[key].name;
+    tmpObj.race = props.careers[key].race;
+    tmpObj.faction = props.careers[key].faction;
+    tmpObj.shortName = key;
+    careersArray.push(tmpObj);
+  }
+  console.log('careers array before', careersArray);
+  // Sort array
+  careersArray.sort(function(a,b) {
+    console.log('a', a.race);
+    console.log('b', b.race);
+    return a.race > b.race;
+  });
+  console.log('careers array after', careersArray);
+  const renderItem = (obj) => {
+    const clickItem = () => {
+      props.gaCareerSelected(obj.shortName);
+    };
+    const url = `/career/${obj.shortName}`;
+    const imgUrl = `/images/icons/${obj.shortName}.png`;
+    return (
+      <Link key={obj.shortName} className={css.item} to={url} onClick={clickItem}>
+        <img src={imgUrl} className={css.icon} />{obj.name}
+      </Link>
+    );
+    //<SidebarItem key={key[0]} gaCareerSelected={props.gaCareerSelected} shortName={key[0]} careerName={key[1].name} />
+  }
   const sidebarClass = classNames({
     [css.container]: !props.sidebar.visible,
     [css.containerActive]: props.sidebar.visible,
@@ -32,7 +43,7 @@ const Sidebar = (props) => {
   return (
     <div className={sidebarClass}>
       <div className={css.content}>
-        {Object.keys(props.careers).map(renderItem)}
+        {careersArray.map(renderItem)}
       </div>
     </div>
   );
