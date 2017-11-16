94c64f14947b8580a2f6cce8149c7b0225f0f414
Matt Richards <matt@kreative.co.uk>
2017-11-16 00:10:20 +0000

    Refactors CareerItem component.

--- src/js/components/CareerItem.js
+++ src/js/components/CareerItem.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import css from '../../css/components/CareerItem.css';
+import { gaCareerSelected } from '../helpers/googleAnalytics';
 
-const SidebarItem = (props) => {
+const CareerItem = (props) => {
   const clickItem = () => {
-    props.gaCareerSelected(props.shortName);
+    gaCareerSelected(props.career.name, props.career.class, props.career.race);
   };
-  const url = `/career/${props.shortName}`;
-  const imgUrl = `/images/icons/${props.shortName}.png`;
+  const url = `/career/${props.career.slug}`;
+  const imgUrl = `/images/icons/${props.career.slug}.png`;
   return (
     <div className={css.item}>
       <Link to={url} onClick={clickItem}>
         <img src={imgUrl} className={css.icon} />
       </Link>
       <Link className={css.link} to={url} onClick={clickItem}>
-        {props.careerName}
+        {props.career.name}
       </Link>
     </div>
   );
 };
 
-export default SidebarItem;
+export default CareerItem;
