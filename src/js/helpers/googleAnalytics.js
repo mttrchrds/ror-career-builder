6c2d30a7ced08dc9bced1cc391399eb861d52d87
Matt Richards <matt@kreative.co.uk>
2017-11-16 00:09:23 +0000

    Adds GA helper functions.

---
+++ src/js/helpers/googleAnalytics.js
@@ -0,0 +1,22 @@
+export function gaEvent(category = false, action = false, label = false, value = false) {
+  
+  // Sending to Google Analytics
+  console.log('Sending GA Event', { 'category': category, 'action': action, 'label': label, 'value': value });
+  // ga('send', {
+  //   hitType: 'event',
+  //   eventCategory: category,
+  //   eventAction: action,
+  //   eventLabel: label,
+  //   eventValue: value,
+  // });
+}
+
+export function gaChangeCareer(changeType) {
+  gaEvent('Career changed', changeType);
+}
+
+export function gaCareerSelected(name, careerClass, race) {
+  gaEvent('Career selected', name);
+  gaEvent('Class selected', careerClass);
+  gaEvent('Race selected', race);
+}
