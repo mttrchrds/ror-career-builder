0aa54af7184d1147ea08335f4c5bf1c5488f8f81
Matt Richards <matt@kreative.co.uk>
2017-11-16 00:13:40 +0000

    Updates main reducer with Overlay and Sidebar.

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,8 +1,12 @@
-import { combineReducers } from "redux";
-import ReducerCareers from "./reducerCareers";
+import { combineReducers } from 'redux';
+import ReducerCareers from './reducerCareers';
+import ReducerSidebar from './reducerSidebar';
+import ReducerOverlayShow from './reducerOverlayShow';
 
 const rootReducer = combineReducers({
-  careers: ReducerCareers
+  careers: ReducerCareers,
+  sidebar: ReducerSidebar,
+  overlayShow: ReducerOverlayShow
 });
 
 export default rootReducer;
