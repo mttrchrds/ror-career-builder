589a7b8251d3570030db5512b20eb9c68c18a338
Matt Richards <matt@kreative.co.uk>
2017-11-19 22:14:29 +0000

    Updates React, React DOM and Hot Loader to latest.

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
       {
         test: /\.js$/,
         loaders: [
-          'react-hot',
+          'react-hot-loader/webpack',
           'babel?presets[]=react,presets[]=es2015'
         ],
         exclude: 'node_modules',
