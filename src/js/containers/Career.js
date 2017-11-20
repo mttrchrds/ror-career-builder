6573d9da044058a73bc813153234d1f4596c877f
Matt Richards <matt@kreative.co.uk>
2017-11-20 00:27:12 +0000

    Resets abilities array when new career selected to enforce loading anim.

--- src/js/containers/Career.js
+++ src/js/containers/Career.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import classNames from 'classnames';
 import css from '../../css/components/Career.css';
 
-import { fetchAbilities } from '../actions/actionAbilities';
+import { fetchAbilities, resetAbilities } from '../actions/actionAbilities';
 import { fetchCareers } from '../actions/actionCareers';
 import { setSlug } from '../actions/actionSlug';
 
@@ -34,7 +34,9 @@ class Career extends Component {
     if (this.props.match.params && (this.props.match.params.slug != nextProps.match.params.slug)) {
       // Check if it's a valid career name
       if (this.props.careers.hasOwnProperty(nextProps.match.params.slug)) {
-
+        
+        // Reset abilities array to force the loading animation
+        this.props.resetAbilities();
         this.loadCareerData(nextProps.match.params.slug);
 
       } else {
@@ -57,16 +59,19 @@ class Career extends Component {
       [css.wrapperSidebar]: this.props.sidebar,
     });
 
-    // TODO: add this to logic after abilities have been imported in Reducer || (!this.props.abilities || this.props.abilities.length == 0)
-    // TODO: also have a check in here that abilities match the slug name somehow. Probably the reason why "Loading..." not appearing
-    // TODO: maybe we could reset the abilities prop to [] when changing career and the above check would still work...?
-    let hasCareerLoaded = (this.props.careers && Object.keys(this.props.careers).length > 0) && this.props.slug;
+    let hasCareerLoaded = (this.props.careers && Object.keys(this.props.careers).length > 0) 
+                          && this.props.slug
+                          && (this.props.abilities && this.props.abilities.length > 0);
 
-    // Display loading component if the corresponding JSON hasn't yet loaded
+    // Display loading component if the relevant JSON hasn't yet loaded
     if (!hasCareerLoaded) {
       return (
-        <div className={css.loadingContainer}>
-          <Loading />
+        <div className={containerClass}>
+          <div className={css.loadingContainer}>
+            <Loading />
+          </div>
+          <Overlay overlayVisible={true} />
+          <Sidebar />
         </div>
       );
     }
@@ -96,4 +101,4 @@ function mapStateToProps({ sidebar, abilities, careers, slug }) {
   };
 }
 
-export default connect(mapStateToProps, { fetchAbilities, fetchCareers, setSlug })(Career);
+export default connect(mapStateToProps, { fetchAbilities, resetAbilities, fetchCareers, setSlug })(Career);
