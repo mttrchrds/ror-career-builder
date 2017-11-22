a227d588f837e2fc96436aca861e0099791bac89
Matt Richards <matt@kreative.co.uk>
2017-11-22 22:19:22 +0000

    Adds Redux Devtools.

--- src/js/entry.js
+++ src/js/entry.js
@@ -2,27 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import promise from "redux-promise";
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import '../css/entry.css';
+
 // For Google Analytics
 import 'autotrack';
 import '../../analytics.js';
-import '../css/entry.css';
 
-import reducers from "./reducers";
+import rootReducer from "./reducers";
 
-/*
-  Import Components
-*/
+//  Import Components
 import App from './components/App';
 import Home from './containers/Home';
 import Career from './containers/Career';
 import NotFound from './components/NotFound';
 
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+// Create store, apply middlewares etc
+const store = createStore(
+	rootReducer,
+	composeWithDevTools(
+		applyMiddleware(promise)
+	)
+);
 
 ReactDOM.render(
-	<Provider store={createStoreWithMiddleware(reducers)}>
+	<Provider store={store}>
 		<Router>
 			<Switch>
 				<Route path="/career/:slug" component={Career} />
