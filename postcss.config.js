b0f1b26f3ca573314cb7f90e045b0419cc6e415e
Matt Richards <matt@kreative.co.uk>
2017-12-05 23:42:04 +0000

    Adjusts PostCSS config tp ensure calc() works.

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
-    require('postcss-calc')(
-      { mediaQueries: true }
-    ),
     require('postcss-modules-values'),
     require('postcss-nested'),
     require('autoprefixer'),
+    require('postcss-calc')(
+      { mediaQueries: true }
+    ),
     require('cssnano')(
       { preset: 'default' }
     )
