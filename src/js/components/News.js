95d1aeaeec967ec2bff5fbf6ff89b8fe663bd4f6
Matt Richards <matt@kreative.co.uk>
2018-03-30 22:22:08 +0100

    Loads News data from S3 and renders.

--- src/js/components/News.js
+++ src/js/components/News.js
@@ -1,40 +1,64 @@
 import React from 'react';
-import IconChevronRight from '../icons/IconChevronRight';
+import axios from 'axios';
+import moment from 'moment';
 import css from '../../css/components/News.css';
+import { staticPath } from '../../../config';
 
-const News = () =>
-  <div className={css.container}>
-    <div className={css.heading}>Latest updates</div>
-    <div className={css.item}>
-      <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
-      <div className={css.itemDate}>4/11/2017</div>
-      <span className={css.itemText}>Updates careers inline with <a href="https://www.returnofreckoning.com/forum/viewtopic.php?f=42&t=23732" target="blank">latest patch</a>.</span>
-    </div>
-    <div className={css.item}>
-      <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
-      <div className={css.itemDate}>4/11/2017</div>
-      <span className={css.itemText}>A couple of UI improvements: Sidebar menu is now ordered in a sane manner, Mastery tree and abilities are now clickable.</span>
-    </div>
-    <div className={css.item}>
-      <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
-      <div className={css.itemDate}>30/10/2017</div>
-      <span className={css.itemText}>Major update of skills and abilities of all careers.</span>
-    </div>
-    <div className={css.item}>
-      <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
-      <div className={css.itemDate}>12/6/2016</div>
-      <span className={css.itemText}>A number of UI improvements to Mastery area.</span>
-    </div>
-    <div className={css.item}>
-      <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
-      <div className={css.itemDate}>10/6/2016</div>
-      <span className={css.itemText}>First major update. Mastery abilities can now be activated independent of selection. Interface reskinned.</span>
-    </div>
-    <div className={css.item}>
-      <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
-      <div className={css.itemDate}>1/6/2016</div>
-      <span className={css.itemText}>We are live :)</span>
-    </div>
-  </div>;
+import IconChevronRight from '../icons/IconChevronRight';
+import Loading from '../containers/Loading';
+
+class News extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      entries: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`${staticPath}json/news.json`)
+      .then((response) => {
+        this.setState({ entries: response.data.entries });
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  renderEntry(entry) {
+    return (
+      <div className={css.item}>
+        <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
+        <div className={css.itemDate}>{moment(entry.date).format('D/MM/YYYY')}</div>
+        <span className={css.itemText}>{entry.text}</span>
+      </div>
+    )
+  }
+
+  renderEntries() {
+    if (this.state.entries.length > 0) {
+      return this.state.entries.map((entry) => {
+        return this.renderEntry(entry);
+      })
+    } else {
+      return (
+        <div className={css.loadingContainer}>
+          <Loading />
+        </div>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <div className={css.container}>
+        <div className={css.heading}>Latest updates</div>
+        {this.renderEntries()}
+      </div>
+    );
+  }
+}
 
 export default News;
